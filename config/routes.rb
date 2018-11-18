2cdddf7592526ce41f2f2c5d9bdda90d124508a6
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 16:02:14 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resource :comments do
     get ':id', to: 'comments#show'
-    get ':id/like', to: 'comments#like', as: 'like'
-    get ':id/dislike', to: 'comments#dislike', as: 'dislike'
+    get ':id/like', to: 'comments#like', as: 'like'          # GET ?! Toutes les routes qui écrivent dans la base 
+    get ':id/dislike', to: 'comments#dislike', as: 'dislike' # DOIVENT utiliser POST/PUT/DELETE, jamais GET !
   end
 end
